47b5cc188205d8f2720006d644274f1c8cc1042a
Adity_Khisa <113276761+adity1234567@users.noreply.github.com>
2023-06-10 22:20:59 +0600

    Add files via upload

---
+++ python turtle graphics/1_shape_color.py
@@ -0,0 +1,18 @@
+import turtle
+shape=turtle.Turtle()
+#model name and the object..
+shape.shape("turtle")
+shape.color()
+('black','red')
+#turtle color-->red
+shape.forward(100)
+shape.color("red")
+shape.forward(100)
+shape.color("blue","green")
+#turtle color-->green
+shape.backward(300)
+
+#using rgb
+#---------------
+turtle.colormode(255)
+shape.color(120,40,50)
